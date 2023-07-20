7bd09158778629c19456b412a3094937c84516d4
redm27 <130718216+redm27@users.noreply.github.com>
2023-07-20 12:08:42 +0900

    Update aa.py

--- aa.py
+++ aa.py
@@ -12,35 +12,45 @@ from cv_bridge import CvBridge
 from tf.transformations import euler_from_quaternion
 
 rospy.init_node('best_driver’)
+
 rospy.Subscriber("/usb_cam/image_raw/", Image, cam_callback)
 rospy.Subscriber('xycar_ultrasonic', Int32MultiArray, ultra_callback)
 rospy.Subscriber("imu", Imu, imu_callback)
 motor_publisher = rospy.Publisher('xycar_motor', xycar_motor, queue_size=1)
 
+
 image = np.empty(shape=[0])
 bridge = CvBridge() 
+
 def cam_callback(data):
 global image
 image = bridge.imgmsg_to_cv2(data, "bgr8")
 
+
 ultra_msg = [0,0,0,0,0,0,0,0]
+
 def ultra_callback(data):
 global ultra_msg
 ultra_msg = data.data
 
+
 imu_msg = None
+
 def imu_callback(data):
 global imu_msg, roll, pitch, yaw
 imu_msg = [data.orientation.x, data.orientation.y, 
 data.orientation.z, data.orientation.w]
 
+
 motor_msg = xycar_motor()
 motor_publisher = rospy.Publisher('xycar_motor', xycar_motor, queue_size=1)
+
 def car_drive(angle, speed):
 motor_msg.angle = angle
 motor_msg.speed = speed
 motor_publisher.publish(motor_msg)
 car_drive(30, 20)
+
 
 while not rospy.is_shutdown():
 ...
